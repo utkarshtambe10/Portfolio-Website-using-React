1f3201081107491b320a6a9bb2ecf9938af4be46
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2023-01-19 10:55:45 +0530

    services component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
 
-    <main className="main">
-      <Home />
-      <About />
-      <Skills />
-    </main>
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+      </main>
     </>
   );
 }
